abe58aef3b9bc79eb7c3aa241a0d081fe4780425
Liza Ramo <liza.h.ramo@gmail.com>
2017-01-30 18:31:54 -0500

    added fade in

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
   console.log("hello this is dog.");
 
-  // set header & mock div height on load & window resize
+  $(".flex-container").css("display", "flex");
+
+  // set height onload && window resize
   $(window).on("resize", function () {
     var height = $(window).height();
+    $(".mock-div").css('height', height);
     $('.flex-container').css('height', height);
   }).resize();
+
+
+  // fade to reveal, then hide so that <a> links are clickable
+  $(".mock-div").delay(400).animate({"opacity": "0"}, 1000, function() {
+    $(".mock-div").hide();
+  }); // end mock div reveal
 
 
 }); // end doc ready
